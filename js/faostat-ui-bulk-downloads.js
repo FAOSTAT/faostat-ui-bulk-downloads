9580d7b4b6256150aad579efeb397f17784ec62c
Kalimaha <guido.barbaglia@gmail.com>
2015-04-22 16:22:15 +0200

    WDS Client.

--- js/faostat-ui-bulk-downloads.js
+++ js/faostat-ui-bulk-downloads.js
@@ -40,50 +40,27 @@ define(['jquery',
         /* this... */
         var _this = this;
 
-        /* Bulk downloads service URL. */
-        var url = this.CONFIG.url_bulk_downloads + '/' + this.CONFIG.datasource + '/' + this.CONFIG.domain + '/' + this.CONFIG.lang;
+        /* Fetch available bulk downloads. */
+        Commons.wdsclient('bulkdownloads', this.CONFIG, function(json) {
 
-        $.ajax({
-
-            url: url,
-            type: 'GET',
-            dataType: 'json',
-
-            success: function (response) {
-
-                /* Cast the result, if required. */
-                var json = response;
-                if (typeof json == 'string')
-                    json = $.parseJSON(response);
-
-                /* Create flat list. */
-                var s = '';
-                var source = $(templates).filter('#dropdown_item').html();
-                var template = Handlebars.compile(source);
-                for (var i = 0 ; i < json.length ; i++) {
-                    var name = json[i][3].replace(/\_/g,' ');
-                    name = name.substring(0, name.indexOf('('));
-                    var size = json[i][3].substr(json[i][3].indexOf('('), json[i][3].indexOf(')'));
-                    var dynamic_data = {
-                        item_url: _this.CONFIG.bulk_downloads_root + json[i][2],
-                        item_text: name,
-                        item_size: size
-                    };
-                    s += template(dynamic_data);
-                }
-
-                /* Render the list. */
-                $('#' + _this.CONFIG.placeholder_id).html(s);
-
-            },
-
-            error: function (a, b, c) {
-                swal({
-                    title: translate.error,
-                    type: 'error',
-                    text: a
-                });
+            /* Create flat list. */
+            var s = '';
+            var source = $(templates).filter('#dropdown_item').html();
+            var template = Handlebars.compile(source);
+            for (var i = 0 ; i < json.length ; i++) {
+                var name = json[i][3].replace(/\_/g,' ');
+                name = name.substring(0, name.indexOf('('));
+                var size = json[i][3].substr(json[i][3].indexOf('('), json[i][3].indexOf(')'));
+                var dynamic_data = {
+                    item_url: _this.CONFIG.bulk_downloads_root + json[i][2],
+                    item_text: name,
+                    item_size: size
+                };
+                s += template(dynamic_data);
             }
+
+            /* Render the list. */
+            $('#' + _this.CONFIG.placeholder_id).html(s);
 
         });
 
@@ -94,55 +71,32 @@ define(['jquery',
         /* this... */
         var _this = this;
 
-        /* Bulk downloads service URL. */
-        var url = this.CONFIG.url_bulk_downloads + '/' + this.CONFIG.datasource + '/' + this.CONFIG.domain + '/' + this.CONFIG.lang;
+        /* Fetch available bulk downloads. */
+        Commons.wdsclient('bulkdownloads', this.CONFIG, function(json) {
 
-        $.ajax({
+            /* Load button template. */
+            var source = $(templates).filter('#dropdown_button').html();
+            var template = Handlebars.compile(source);
+            var dynamic_data = {
+                button_label: translate.button_label
+            };
+            var html = template(dynamic_data);
+            $('#' + _this.CONFIG.placeholder_id).html(html);
 
-            url: url,
-            type: 'GET',
-            dataType: 'json',
-
-            success: function (response) {
-
-                /* Cast the result, if required. */
-                var json = response;
-                if (typeof json == 'string')
-                    json = $.parseJSON(response);
-
-                /* Load button template. */
-                var source = $(templates).filter('#dropdown_button').html();
-                var template = Handlebars.compile(source);
-                var dynamic_data = {
-                    button_label: translate.button_label
+            /* Create entries for the item list. */
+            var s = '';
+            source = $(templates).filter('#dropdown_item').html();
+            template = Handlebars.compile(source);
+            for (var i = 0 ; i < json.length ; i++) {
+                dynamic_data = {
+                    item_url: _this.CONFIG.bulk_downloads_root + json[i][2],
+                    item_text: json[i][3].replace(/\_/g,' ')
                 };
-                var html = template(dynamic_data);
-                $('#' + _this.CONFIG.placeholder_id).html(html);
-
-                /* Create entries for the item list. */
-                var s = '';
-                source = $(templates).filter('#dropdown_item').html();
-                template = Handlebars.compile(source);
-                for (var i = 0 ; i < json.length ; i++) {
-                    dynamic_data = {
-                        item_url: _this.CONFIG.bulk_downloads_root + json[i][2],
-                        item_text: json[i][3].replace(/\_/g,' ')
-                    };
-                    s += template(dynamic_data);
-                }
-
-                /* Append items to the button. */
-                $('#item_list').append(s);
-
-            },
-
-            error: function (a, b, c) {
-                swal({
-                    title: translate.error,
-                    type: 'error',
-                    text: a
-                });
+                s += template(dynamic_data);
             }
+
+            /* Append items to the button. */
+            $('#item_list').append(s);
 
         });
 
