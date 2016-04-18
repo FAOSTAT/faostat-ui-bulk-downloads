a87eec7a6cbef9958e2bfab60a53f21326d0e0bf
simonemurzilli <simone.murzilli@fao.org>
2016-04-18 17:52:11 +0200

    .

--- nls/es/translate.js
+++ nls/es/translate.js
@@ -3,7 +3,7 @@ define({
 
     courtesy: 'There are no bulk downloads available for the selected FAOSTAT code.<br>We apologize for any inconvenience.',
     no_bulk_downlaod_available: 'There are no bulk downloads available for the selected FAOSTAT Domain.<br>We apologize for any inconvenience.',
-    bulk_downloads: 'Bulk Downloads',
+    bulk_downloads: 'Todos Los Datos',
     bulk_downloads_welcome: 'Use this section to download all the domain related data in bulk.'
 
 });
